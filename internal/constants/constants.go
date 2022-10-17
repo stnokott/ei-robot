a8921c59d7ffb9227a298640d58b55902a2ddc8e
stnokott <nftrash@web.de>
2022-10-17 11:34:52 +0200

    feat: implement egg deletion process

--- internal/constants/constants.go
+++ internal/constants/constants.go
@@ -3,22 +3,32 @@ package constants
 import (
 	"fmt"
 	"strings"
+	"time"
 )
 
 const VERSION = "v0.1.0"
 
 const (
-	CMD_START  string = "/start"
-	CMD_NEWEGG string = "/neu"
-	CMD_GETEGG string = "/info"
+	CMD_START     string = "/start"
+	CMD_NEWEGG    string = "/neu"
+	CMD_GETEGG    string = "/info"
+	CMD_DELETEEGG string = "/del"
+	REPLY_YES     string = "Ja"
+	REPLY_NO      string = "Nein"
 )
 
 const DATE_LAYOUT = `02\.01\.2006`
 
+func FormatDate(t time.Time) string {
+	return t.Format(DATE_LAYOUT)
+}
+
 var MSG_HELP string = fmt.Sprintf(`🥚 Ei, Ro*BOT* 🤖 \(%s\)
 %s \- Diese Hilfe anzeigen
 %s \- Neue Eier registrieren
-%s \- Ablaufdatum erfahren`, strings.ReplaceAll(VERSION, `.`, `\.`), CMD_START, CMD_NEWEGG, CMD_GETEGG)
+%s \- Ablaufdatum erfahren
+%s \- Registrierte Eier löschen
+`, strings.ReplaceAll(VERSION, `.`, `\.`), CMD_START, CMD_NEWEGG, CMD_GETEGG, CMD_DELETEEGG)
 
 const MSG_UNKNOWN_COMMAND = `Unbekanntes Kommando\.
 Versuche /start für eine kurze Übersicht aller Kommandos\.`
@@ -30,6 +40,15 @@ const MSG_INVALID_DATE = `Das ist keine gültige Datumsangabe\. Bitte versuche e
 
 const MSG_DATE_SAVED = `*%s* erfolgreich gespeichert\.`
 
+// FIXME: remove code ticks
 const MSG_NO_EGG = "Du hast noch keine Eier registriert\\. Verwende dafür `" + CMD_NEWEGG + "`\\."
 
-const MSG_EGG_INFO = `Deine Eier laufen am %s ab\.`
+const MSG_EGG_INFO = `Deine Eier laufen am *%s* ab\.`
+
+const MSG_REQUEST_DEL_CONFIRM = `Deine Eier laufen am *%s* ab\. Bist du sicher, dass du das löschen möchtest?`
+
+const MSG_INVALID_CONFIRMATION = `Ungültige Eingabe, bitte benutze die vorgegebenen Buttons\.`
+
+const MSG_DELETED = `Erfolgreich gelöscht\.`
+
+const MSG_CANCELLED = `Abgebrochen\.`
--- internal/logic/logic.go
+++ internal/logic/logic.go
@@ -7,15 +7,25 @@ import (
 )
 
 const (
-	STATE_IDLE            string = "STATE_IDLE"
-	STATE_WAIT_DATE       string = "STATE_WAIT_DATE"
-	TRANS_UNKNOWN         string = "TRANS_UNKNOWN"
-	TRANS_START           string = "TRANS_START"
-	TRANS_NEW_EGG         string = "TRANS_NEW_EGG"
-	TRANS_SET_DAY_VALID   string = "TRANS_SET_DAY"
-	TRANS_SET_DAY_INVALID string = "TRANS_SET_DAY_INVALID"
-	TRANS_GET_EGG_INFO    string = "TRANS_GET_EGG_INFO"
+	STATE_IDLE                 string = "STATE_IDLE"
+	STATE_WAIT_DATE            string = "STATE_WAIT_DATE"
+	STATE_WAIT_DEL_CONFIRM     string = "STATE_WAIT_DEL_CONFIRM"
+	TRANS_UNKNOWN              string = "TRANS_UNKNOWN"
+	TRANS_SILENT_CANCEL        string = "TRANS_SILENT_CANCEL"
+	TRANS_START                string = "TRANS_START"
+	TRANS_NEW_EGG              string = "TRANS_NEW_EGG"
+	TRANS_SET_DAY_VALID        string = "TRANS_SET_DAY"
+	TRANS_SET_DAY_INVALID      string = "TRANS_SET_DAY_INVALID"
+	TRANS_GET_EGG_INFO         string = "TRANS_GET_EGG_INFO"
+	TRANS_DEL_EGG              string = "TRANS_DEL_EGG"
+	TRANS_DEL_EGG_NO_EGG       string = "TRANS_DEL_EGG_NO_EGG"
+	TRANS_DEL_EGG_CANCEL       string = "TRANS_DEL_EGG_CANCEL"
+	TRANS_YES                  string = "TRANS_YES"
+	TRANS_NO                   string = "TRANS_NO"
+	TRANS_INVALID_CONFIRMATION string = "TRANS_INVALID_CONFIRMATION"
 )
+
+// TODO: /cancel
 
 var events = fsm.Events{
 	{Name: TRANS_UNKNOWN, Src: []string{STATE_IDLE}, Dst: STATE_IDLE},
@@ -24,6 +34,12 @@ var events = fsm.Events{
 	{Name: TRANS_SET_DAY_VALID, Src: []string{STATE_WAIT_DATE}, Dst: STATE_IDLE},
 	{Name: TRANS_SET_DAY_INVALID, Src: []string{STATE_WAIT_DATE}, Dst: STATE_WAIT_DATE},
 	{Name: TRANS_GET_EGG_INFO, Src: []string{STATE_IDLE}, Dst: STATE_IDLE},
+	{Name: TRANS_DEL_EGG, Src: []string{STATE_IDLE}, Dst: STATE_WAIT_DEL_CONFIRM},
+	{Name: TRANS_YES, Src: []string{STATE_WAIT_DEL_CONFIRM}, Dst: STATE_IDLE},
+	{Name: TRANS_NO, Src: []string{STATE_WAIT_DEL_CONFIRM}, Dst: STATE_IDLE},
+	{Name: TRANS_DEL_EGG_NO_EGG, Src: []string{STATE_IDLE}, Dst: STATE_IDLE},
+	{Name: TRANS_INVALID_CONFIRMATION, Src: []string{STATE_WAIT_DEL_CONFIRM}, Dst: STATE_WAIT_DEL_CONFIRM},
+	{Name: TRANS_SILENT_CANCEL, Src: []string{STATE_IDLE}, Dst: STATE_IDLE},
 }
 
 type FSM struct {
@@ -48,14 +64,19 @@ func (f *FSM) Current() string {
 type TelegramCb func()
 
 type TelegramCbs struct {
-	OnUnknownCmd  TelegramCb
-	OnStartCmd    TelegramCb
-	OnNewEggCmd   TelegramCb
-	OnInvalidDate TelegramCb
-	OnGetEggInfo  TelegramCb
+	OnUnknownCmd          TelegramCb
+	OnStartCmd            TelegramCb
+	OnNewEggCmd           TelegramCb
+	OnInvalidDate         TelegramCb
+	OnGetEggInfo          TelegramCb
+	OnDelEggRequest       TelegramCb
+	OnDelEggConfirm       TelegramCb
+	OnDelEggCancel        TelegramCb
+	OnDelEggNoEgg         TelegramCb
+	OnInvalidConfirmation TelegramCb
 }
 
-func NewFSM(cbs TelegramCbs) *FSM {
+func NewFSM(cbs *TelegramCbs) *FSM {
 	return &FSM{
 		fsm.NewFSM(
 			STATE_IDLE,
@@ -66,6 +87,24 @@ func NewFSM(cbs TelegramCbs) *FSM {
 				TRANS_NEW_EGG:         func(_ *fsm.Event) { cbs.OnNewEggCmd() },
 				TRANS_SET_DAY_INVALID: func(_ *fsm.Event) { cbs.OnInvalidDate() },
 				TRANS_GET_EGG_INFO:    func(_ *fsm.Event) { cbs.OnGetEggInfo() },
+				TRANS_DEL_EGG:         func(_ *fsm.Event) { cbs.OnDelEggRequest() },
+				TRANS_YES: func(e *fsm.Event) {
+					if e.Src == STATE_WAIT_DEL_CONFIRM {
+						cbs.OnDelEggConfirm()
+					} else {
+						cbs.OnUnknownCmd()
+					}
+				},
+				TRANS_NO: func(e *fsm.Event) {
+					if e.Src == STATE_WAIT_DEL_CONFIRM {
+						cbs.OnDelEggCancel()
+					} else {
+						cbs.OnUnknownCmd()
+					}
+				},
+				TRANS_DEL_EGG_NO_EGG:       func(_ *fsm.Event) { cbs.OnDelEggNoEgg() },
+				TRANS_DEL_EGG_CANCEL:       func(_ *fsm.Event) { cbs.OnDelEggCancel() },
+				TRANS_INVALID_CONFIRMATION: func(_ *fsm.Event) { cbs.OnInvalidConfirmation() },
 			},
 		),
 	}
